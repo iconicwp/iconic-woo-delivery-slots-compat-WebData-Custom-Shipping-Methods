7dc096ff50ec0725aca4f804d20cbb88bde31089
Pramod Jodhani <mrpramodjodhani@gmail.com>
2021-04-05 12:12:25 +0530

    first commit

---
+++ iconic-woo-delivery-slots-compat-custom-shipping-webdata.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Plugin Name:     WooCommerce Delivery Slots by Iconic [WebData Custom Shipping Methods for WooCommerce]
+ * Plugin URI:      https://iconicwp.com/products/woocommerce-delivery-slots/
+ * Description:     Compatibility between WooCommerce Delivery Slots by Iconic and Custom Shipping Methods for WooCommerce by WebData.
+ * Author:          Iconic
+ * Author URI:      https://iconicwp.com/
+ * Text Domain:     iconic-woo-delivery-slots-compat-shipping-pro
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Iconic_Woo_Delivery_Slots_Compat_Custom_Shipping_Webdata
+ */
+
+/**
+ * Is Custom Shipping Methods for WooCommerce active?
+ *
+ * @return bool
+ */
+function iconic_compat_wb_custom_shipping_is_active() {
+	return function_exists( 'WB_Custom_WooCommerce_Shipping_Method_Init' );
+}
+
+
+/**
+ * Change format of the shipping method ID.
+ *
+ * @return array
+ */
+function iconic_compat_wb_custom_shipping_update_shipping_method_id( $shipping_method_options ) {
+	if ( ! iconic_compat_wb_custom_shipping_is_active() ) {
+		return $shipping_method_options;
+	}
+
+	$updated_shipping_method = array();
+
+	foreach ( $shipping_method_options as $method_key => $method_name ) {
+		if ( false !== strpos( $method_key, 'wb_custom_woocommerce_shipping_method:' ) ) {
+			$method_key = str_replace( 'wb_custom_woocommerce_shipping_method:', 'WB_Custom_WooCommerce_Shipping_Method', $method_key );
+		}
+
+		$updated_shipping_method[ $method_key ] = $method_name;
+	}
+
+	return $updated_shipping_method;
+}
+
+add_filter( 'iconic_wds_shipping_method_options', 'iconic_compat_wb_custom_shipping_update_shipping_method_id', 10 );
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Description
+
+This is a compatibility plugin for [WooCommerce Delivery Slots](https://iconicwp.com/products/woocommerce-delivery-slots/?utm_source=Iconic&utm_medium=Github&utm_campaign=iconic-woo-delivery-slots-compat-shipping-pro)  by Iconic and [Custom Shipping Methods for WooCommerce](https://web-data.online/docs/woocommerce-custom-shipping-methods/) by WebData.
+
+# Changelog
+
+## 0.1.0 (2021-04-05)
+* Initial Release.
